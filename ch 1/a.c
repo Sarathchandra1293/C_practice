8926191cc11bd304d046d4fdec738a788f04fd49
sarathchandra edubelli <99632999+Sarathchandra1293@users.noreply.github.com>
2023-12-23 11:25:44 +0530

    Add files via upload

---
+++ ch 1/a.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main( )
+{
+int m1, m2, m3;
+printf( "Enter values of marks in 3 subjects\n" );
+scanf( "%d %d %d", &m1, &m2, &m3 );
+printf( "You entered %d %d %d", m1, m2, m3 ) ;
+
+return 0;
+}
---
+++ ch 1/d.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+    float b;
+    int c ;
+    a = 25 ; b = 3.24 ; 
+    c = a + b * b - 35 ;
+    printf("%d",c);
+return 0;
+}
